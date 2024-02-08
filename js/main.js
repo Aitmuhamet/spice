a944ce722daed391324d2d962c000881a1a9b4ff
Aitmuhamet Hojakeldiyev <92148785+Aitmuhamet@users.noreply.github.com>
2024-02-08 20:39:08 +0500

    Added the first section of the Main section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var toggleButton = document.getElementById('toggleButton');
-    var itemsContainer = document.querySelector('.items-container');
-  
+    let toggleButton = document.querySelector('.main-restaurant__more');
+    let itemsList = document.querySelector('.main-restaurant__list');
+    let restaurants = itemsList.querySelectorAll('.main-restaurant__item')
+    
+    restaurants.forEach(function(elem, key){
+      if (key > 1) {
+        elem.classList.add('main-restaurant__item--hidden');
+        elem.classList.remove('main-restaurant__item--show');
+        // elem.style.opacity = 0;
+      } else {
+        elem.classList.add('main-restaurant__item--show');
+        // elem.style.opacity = 1;
+      }
+    })
+    
     toggleButton.addEventListener('click', function () {
-      itemsContainer.classList.toggle('expanded');
+      itemsList.classList.toggle('main-restaurant__list--expand');
+      restaurants.forEach(function(elem, key){
+        if (key > 1) {
+          console.log(elem.style.opacity);
+          elem.classList.toggle('main-restaurant__item--hidden');
+          elem.classList.toggle('main-restaurant__item--show');
+          // elem.style.opacity = 1;
+        } 
+      })
     });
   });
